74725971feccd639bee98aa84f3efa0a9e9760a6
okupara <okuharakensuke@gmail.com>
2019-07-27 04:08:06 +0900

    Fix tsc failed

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import Test from './components/Test'
 
-ReactDOM.render(<Test />, document.getElementById('root'))
+ReactDOM.render(<div />, document.getElementById('root'))
