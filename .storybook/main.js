b216532f48bccf369b0ce0a4b21751cda8d7f15c
okupara <okuharakensuke@gmail.com>
2020-02-13 02:11:25 +0900

    Be able to use Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,21 @@
+const path = require('path');
+module.exports = {
+  stories: ['../stories/**/*.stories.tsx'],
+  addons: [
+    {
+      name: '@storybook/preset-typescript',
+      options: {
+        tsLoaderOptions: {
+          configFile: path.resolve(__dirname, './tsconfig.json'),
+        },
+        tsDocgenLoaderOptions: {
+          tsconfigPath: path.resolve(__dirname, './tsconfig.json'),
+        },
+        include: [
+          path.resolve(__dirname, "../stories"),
+          path.resolve(__dirname, "../src"),
+        ],
+      },
+    },
+  ],
+};
---
+++ stories/test.stories.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Test from "../src/app/components/Test"
+
+export const test = () => <Test />
+
+export default {
+    title: "test"
+}
