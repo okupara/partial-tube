2ba50fa7492c1f8465faf2eb181a3be7bce74dd5
okupara <okuharakensuke@gmail.com>
2020-03-22 21:32:54 +0900

    Setup the basic part of GraphQL

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -1,15 +1,32 @@
 import React from "react"
 import AuthApp from "../components/AuthApp"
+import fetch from "isomorphic-unfetch"
 // import App from "../components/App"
 // import { LoginUserProvider } from "../context/LoginUser"
 
 const Index = () => (
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
   <div>
     <AuthApp>
       <div>konnnichiha!</div>
     </AuthApp>
   </div>
 )
+
+Index.getInitialProps = async () => {
+  const response = await fetch("http://localhost:3000/api/graphql", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ query: "{ users { name } }" }),
+  })
+
+  const {
+    data: { users },
+  } = await response.json()
+  console.log("test", users)
+
+  return { users }
+}
 
 export default Index
